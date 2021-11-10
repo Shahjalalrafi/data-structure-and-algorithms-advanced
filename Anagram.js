048cad10e415c94e4742a6863bea3d838baf75ed
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-11-10 11:59:12 +0600

    anagram string

---
+++ Anagram.js
@@ -0,0 +1,29 @@
+function ValidAnagram(str1, str2) {
+    if (str1.length !== str2.length) {
+        return false
+    }
+
+    let lookup = {}
+    for (let i = 0; i < str1.length; i++) {
+        let letter = str1[i]
+        {if (lookup[letter]) {
+            lookup[letter] += 1
+        } else {
+            lookup[letter] = 1
+        }}
+    }
+    
+    for(let i = 0; i< str2.length; i++) {
+        let letter = str2[i]
+        if(!lookup[letter]) {
+            return false
+        }else {
+            lookup[letter] -= 1
+        }
+    }
+    return true
+}
+
+
+// complexity o(n)
+console.log(ValidAnagram("hello", "ohell"))
