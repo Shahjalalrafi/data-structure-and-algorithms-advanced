78f96715a512b7c442bfc9473dea4e108dd6e46b
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-11-18 23:32:45 +0600

    time and spaace coplexity analyse in our recursion code

--- helperMethodRecursion.js
+++ helperMethodRecursion.js
@@ -1,17 +1,23 @@
 
-function findOddNumber(arr) {
-    let result = []
-    function helper(input) {
-        if(input.length === 0) return
+// function findOddNumber(arr) {
+//     let result = []
+//     function helper(input) {
+//         if(input.length === 0) {
+//             return
+//         }
 
-        if(input[0] % 2 !== 0) {
-            result.push(input[0])
-        }
         
-        helper(input.slice(1))
-    }
-    helper(arr)
-    return result
-}
+//         if(input[0] % 2 !== 0) {
+//             result.push(input[0])
+//         }
+        
+//         helper(input.slice(1))
+//     }
+//     helper(arr)
+//     return result
+
+    
+// }
+
 
 console.log(findOddNumber([2, 3, 5, 7, 3,4 ,12, 32, 55, 75]))
