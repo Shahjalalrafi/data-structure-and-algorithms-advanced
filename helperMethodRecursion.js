945c541a2fd4d05826c7b8e9779b25f77ba23231
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-11-29 23:04:38 +0600

    linear search implement

--- helperMethodRecursion.js
+++ helperMethodRecursion.js
@@ -1,23 +1,20 @@
 
-// function findOddNumber(arr) {
-//     let result = []
-//     function helper(input) {
-//         if(input.length === 0) {
-//             return
-//         }
+function findOddNumber(arr) {
 
-        
-//         if(input[0] % 2 !== 0) {
-//             result.push(input[0])
-//         }
-        
-//         helper(input.slice(1))
-//     }
-//     helper(arr)
-//     return result
+    let result = []
+    function helper(input) {
+        if(input.length === 0) return
 
-    
-// }
+        if(input[0] % 2 !== 0) {
+            result.push(input[0])
+        }
 
+        helper(input.slice(1))
+    }
 
-console.log(findOddNumber([2, 3, 5, 7, 3,4 ,12, 32, 55, 75]))
+    helper(arr)
+
+    return result.length
+}
+
+console.log(findOddNumber([2, 3, 5, 7, 3,4 ,12, 32, 55]))
