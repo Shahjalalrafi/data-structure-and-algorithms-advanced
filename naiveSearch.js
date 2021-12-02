c48e9f7f0a6770509cdf3139e47e022425d96261
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-12-02 11:33:19 +0600

    naiveSearch implement

---
+++ naiveSearch.js
@@ -0,0 +1,13 @@
+function naiveSearch(longStr, shortStr) {
+    let count = 0;
+    for(let i = 0; i < longStr.length; i++) {
+        for(let j = 0; j < shortStr.length; j++) {
+            if(shortStr[j] !== longStr[i + j]) break
+
+            if(j === shortStr.length - 1) count++
+        }
+    }
+    return count
+}
+
+console.log(naiveSearch("hello everyone hello", "llo"))
