630712608ac7162dfd957bd994ace9d342dac5d1
Shahjalalrafi <shahjalalrafi5@gmail.com>
2021-12-04 00:19:00 +0600

    selection sort implemented

--- selectionSort.js
+++ selectionSort.js
@@ -1,12 +1,14 @@
 function selectionSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
+    for( let i = 0; i< arr.length; i++) {
         let lowest = i
-        for (let j = i+1; j < arr.length; j++) {
+        for( let j = i + 1; j < arr.length; j++) {
             if(arr[lowest] > arr[j]) {
                 lowest = j
             }
         }
+
         if(i !== lowest) {
+            console.log(arr, i, lowest)
             let temp = arr[i]
             arr[i] = arr[lowest]
             arr[lowest] = temp
@@ -15,4 +17,5 @@ function selectionSort(arr) {
     return arr
 }
 
-console.log(selectionSort([5, 8, 23, -1, 4]))
+
+console.log(selectionSort([0, 1, -1, 3]))
