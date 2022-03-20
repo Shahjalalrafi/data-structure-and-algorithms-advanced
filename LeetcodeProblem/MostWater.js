351c6818a140b6edb775d4a2a2e59a7a26647840
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-03-20 23:21:23 +0600

    most container water leetcode problem solved

--- LeetcodeProblem/MostWater.js
+++ LeetcodeProblem/MostWater.js
@@ -2,52 +2,35 @@
 // Question: https://leetcode.com/problems/container-with-most-water/
 
 
-// var maxArea = function (height) {
-//     if(height.length <= 1) return 0;
-
-//     let maxWater = 0;
-//     for (let i = 0; i < height.length; i++) {
-//         // let len
-//         // let wid
-//         for (j = i + 1; j < height.length; j++) {
-//             // let len = Math.min(height[i], height[j])
-//             let len;
-//              if (height[i] < height[j]) {
-//                 len = height[i]
-//             } else {
-//                 len = height[j]
-//             }
-//             let wid = j - i;
-//             let totalWater = len * wid;
-//             if(maxWater < totalWater) maxWater = totalWater
-//         }
-
-//     }
-//     return maxWater
-// }
-
-
-
 var maxArea = function (heights) {
-    let maxArea = 0;
-    let firstPointer = 0;
-    let lastPointer = heights.length -1;
+    let totalWater = 0;
 
-    while(firstPointer < lastPointer) {
-        const height = Math.min(heights[firstPointer], heights[lastPointer]) ;
-        const width = lastPointer - firstPointer;
+    for (let i = 0; i < heights.length; i++) {
 
-        const area = height * width;
-        maxArea = Math.max(maxArea, area);
+        for (let j = i + 1; j < heights.length; j++) {
+            
+            let height
+            if(heights[i] > heights[j]) {
+                // 1            // 8
+                height = heights[j]
+            }else {
+                height = heights[i]
+            }
 
-        if(heights[firstPointer] <= heights[lastPointer]) {
-            firstPointer++;
-        }else {
-            lastPointer--;
+            let width = j - i
+
+            let currentWater = height * width ;
+            
+            // console.log(totalWater);
+            if(totalWater < currentWater) {
+                totalWater = currentWater;
+            }
+
+            // console.log(currentWater)
         }
+
     }
-    return maxArea;
+    return totalWater;
 }
 
-
-console.log(maxArea([1,8,6,2,5,4,8,3,7]))
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
