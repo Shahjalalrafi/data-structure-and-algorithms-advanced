48c7e27e99b3626c7bc6f5c616dd61ec45a31d43
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-03-22 22:08:15 +0600

    unnecessary things deleted from text file

--- LeetcodeProblem/MostWater.js
+++ LeetcodeProblem/MostWater.js
@@ -2,49 +2,77 @@
 // Question: https://leetcode.com/problems/container-with-most-water/
 
 
+// var maxArea = function (heights) {
+//     let totalWater = 0;
+
+//     for (let i = 0; i < heights.length; i++) {
+
+//         for (let j = i + 1; j < heights.length; j++) {
+
+//             ///////    High level code     ////////
+
+//             let height = Math.min(heights[i], heights[j]);
+//             let width = j - i
+
+//             let currentWater = height * width ;
+//             totalWater = Math.max(currentWater, totalWater);
+
+
+
+
+//             //////   low level code     ////
+
+//             // let height;
+
+//             // if(heights[i] > heights[j]) {
+//             //     // 1            // 8
+//             //     height = heights[j]
+//             // }else {
+//             //     height = heights[i]
+//             // }
+
+
+
+//             // console.log(totalWater);
+//         //    let currentWater = height * width ;
+//             // if(totalWater < currentWater) {
+//             //     totalWater = currentWater;
+//             // }
+
+
+//             // console.log(currentWater)
+//         }
+
+//     }
+//     return totalWater;
+// }
+
+
+
 var maxArea = function (heights) {
-    let totalWater = 0;
+    let maxArea = 0;
+    let firstPointer = 0;
+    let lastPointer = heights.length - 1;
 
-    for (let i = 0; i < heights.length; i++) {
+    while (firstPointer < lastPointer) {
+        const height = Math.min(heights[firstPointer], heights[lastPointer]);
+        const width = lastPointer - firstPointer;
 
-        for (let j = i + 1; j < heights.length; j++) {
+        const area = height * width;
+        maxArea = Math.max(maxArea, area);
 
-            ///////    High level code     ////////
-            
-            let height = Math.min(heights[i], heights[j]);
-            let width = j - i
-
-            let currentWater = height * width ;
-            totalWater = Math.max(currentWater, totalWater);
-
-
-
-
-            //////   low level code     ////
-
-            // let height;
-
-            // if(heights[i] > heights[j]) {
-            //     // 1            // 8
-            //     height = heights[j]
-            // }else {
-            //     height = heights[i]
-            // }
-
-
-            
-            // console.log(totalWater);
-        //    let currentWater = height * width ;
-            // if(totalWater < currentWater) {
-            //     totalWater = currentWater;
-            // }
-
-
-            // console.log(currentWater)
+        if (heights[firstPointer] <= heights[lastPointer]) {
+            firstPointer++;
+        } else {
+            lastPointer--;
         }
-
     }
-    return totalWater;
+    return maxArea;
 }
 
 console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+
+totalWater = 8;
+
+
+// 1 * 8 = 8
