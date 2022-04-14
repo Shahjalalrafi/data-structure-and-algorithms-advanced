c1dd66f68c8e87ca656b219d40616038949cbcbf
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-14 12:28:41 +0600

    queue peek elemente

--- StackQueue/Queue.js
+++ StackQueue/Queue.js
@@ -5,21 +5,20 @@ class Queue {
 
     add(record) {
         this.data.unshift(record);
-        return this
     }
 
     remove() {
         return this.data.pop();
     }
 
-    // peek() {
-    //     return this.data.slice(this.data.length - 1, 1);
-    // }
+    peek() {
+        return this.data[this.data.length - 1];
+    }
 }
 
 const q = new Queue();
 q.add(2)
 q.add(5)
 console.log(q.add(3))
-
-console.log(q.remove())
+console.log(q.peek())
+// console.log(q.remove())
