0d167cb858f1b2fddbd6e8c93b92b5a7654fb4c0
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-20 22:19:59 +0600

    capitalize sentence problem solve

--- CapitalizeSentence.js
+++ CapitalizeSentence.js
@@ -1,3 +1,31 @@
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+
+function capitalize(str) {
+    let results = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        let char = str[i];
+        let beforeChar = str[i-1];
+        if(beforeChar === " ") {
+            results += char.toUpperCase();
+        }else {
+            results += char
+        }
+    }
+
+    return results;
+}
+
+
+// Another solution
 function capitalize(str) {
     let splitStr = str.split(" ");
 
@@ -12,5 +40,8 @@ function capitalize(str) {
 
     return wordArr.join(" ");
 }
+
+
+
 
 console.log(capitalize("a short sentence"))
