785d3df696bb35156251cb92038ee954d5767617
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-23 21:23:21 +0600

    isPalindrome checking with recursive function

--- Recursion/RecursionProblem.js
+++ Recursion/RecursionProblem.js
@@ -5,3 +5,12 @@ function reverse(str){
 	if(str.length <= 1) return str;
 	return reverse(str.slice(1)) + str[0];
 }
+
+// isPalindrome Solution
+function isPalindrome(str){
+    if(str.length === 1) return true;
+    if(str.length === 2) return str[0] === str[1];
+    if(str[0] === str.slice(-1)) return isPalindrome(str.slice(1,-1))
+    return false;
+}
+
