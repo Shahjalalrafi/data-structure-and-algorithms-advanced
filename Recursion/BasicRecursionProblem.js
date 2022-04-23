6335b2f4f2999e42b7f8d63c3cc5ba276f2c6160
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-23 21:36:50 +0600

    fibonacchi sequence problem solve with recursive function

--- Recursion/BasicRecursionProblem.js
+++ Recursion/BasicRecursionProblem.js
@@ -41,3 +41,17 @@ function recursiveRange(num) {
  // SAMPLE INPUT/OUTPUT
 // recursiveRange(6) // 21
 // recursiveRange(10) // 55 
+
+
+function fib(num){
+  // add whatever parameters you deem necessary - good luck!  
+
+    if(num <= 2) return 1;
+    
+    return fib(num-1) + fib(num-2);
+}
+
+// fib(4) // 3
+// fib(10) // 55
+// fib(28) // 317811
+// fib(35) // 9227465
