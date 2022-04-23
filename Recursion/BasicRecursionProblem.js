fb18247014169cd6d72fe4a335881bdb0d3a50cd
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-23 21:33:22 +0600

    basic recursion add recursion helpermethod problem solve

---
+++ Recursion/BasicRecursionProblem.js
@@ -0,0 +1,11 @@
+
+
+// POWER SOLUTION
+function power(base, exponent){
+    if(exponent === 0) return 1;
+    return base * power(base,exponent-1);
+}
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
