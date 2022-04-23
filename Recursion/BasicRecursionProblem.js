5fa9f31c5e947049b3a6bd350454dd842e0d4739
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-23 21:34:03 +0600

    factorial implemented with recursive way

--- Recursion/BasicRecursionProblem.js
+++ Recursion/BasicRecursionProblem.js
@@ -9,3 +9,14 @@ function power(base, exponent){
 // power(2,0) // 1
 // power(2,2) // 4
 // power(2,4) // 16
+
+function factorial(num){
+    if(num === 0) return 1;
+    
+    return num * factorial(num -1);
+ }
+ 
+ //factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
