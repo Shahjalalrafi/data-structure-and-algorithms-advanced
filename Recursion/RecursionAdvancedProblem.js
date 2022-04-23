d66718a92f37f6aaea9c3fe6b34b0b3f0c21a9c6
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-23 21:49:56 +0600

    reverse function input and output setting

--- Recursion/RecursionAdvancedProblem.js
+++ Recursion/RecursionAdvancedProblem.js
@@ -5,6 +5,8 @@ function reverse(str) {
     if (str.length <= 1) return str;
     return reverse(str.slice(1)) + str[0];
 }
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
 
 // isPalindrome Solution
 function isPalindrome(str) {
