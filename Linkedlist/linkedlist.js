09aa40faf4e2f49a52126af9b166f95c937bb272
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-04-24 22:32:32 +0600

    getlast node from our linkedlist

--- Linkedlist/linkedlist.js
+++ Linkedlist/linkedlist.js
@@ -14,13 +14,13 @@ class LinkedList {
     inserFirst(data) {
         this.head = new Node(data, this.head);
     }
-    
+
     // checking how many node have in this list
     size() {
         let counter = 0;
         let node = this.head;
 
-        while(node) {
+        while (node) {
             counter++;
             node = node.next;
         }
@@ -31,6 +31,20 @@ class LinkedList {
     getFirst() {
         return this.head;
     }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node
+            };
+            node = node.next
+        }
+    }
 }
 
 const list = new LinkedList();
@@ -40,5 +54,6 @@ list.inserFirst(5);
 list.inserFirst(75);
 console.log(list.size())
 console.log(list.getFirst())
+console.log(list.getLast())
 
 console.log(list)
