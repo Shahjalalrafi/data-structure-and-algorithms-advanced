bd50f3ab303c711809a761dca16b84f21714bb52
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-05-09 20:20:24 +0600

    removing last node from linked list

--- Linkedlist/linkedlist.js
+++ Linkedlist/linkedlist.js
@@ -28,10 +28,12 @@ class LinkedList {
         return counter;
     }
 
+    // Getting first node from our linkedlist
     getFirst() {
         return this.head;
     }
 
+    // Getting last node from our linkedlist
     getLast() {
         if (!this.head) {
             return null;
@@ -46,16 +48,56 @@ class LinkedList {
         }
     }
 
+    // clear all node from our linkedlist
     clear() {
-        this.head = null;    
+        this.head = null;
     }
 
+    // removing first node from our linkedlist
     removeFirst() {
-        if(!this.head) {
+        if (!this.head) {
             return
         }
 
         this.head = this.head.next;
+    }
+
+
+    // removing first node from our linkedlist
+    removeFirst() {
+        if (!this.head) {
+            return
+        }
+
+        this.head = this.head.next;
+    }
+
+
+    // removing last node from our linkedlist
+    removeLast() {
+        // if no head nothing will be happen
+        if(!this.head) {
+            return;
+        }
+        
+        // if there is not more than one node then one head node will be deleted
+        if(!this.head.next) {
+            this.head = null;
+            return;
+        }
+
+        // refferencing previous and current node
+        let previous = this.head;
+        let node = this.head.next;
+
+        // if next node have then previous node will be current node and current node will be next node;
+        while(node.next) {
+            previous = node;
+            node = node.next;
+        }
+
+        // after iterating whole list current node will be deleted by setting previous.next node = null;
+        previous.next = null;
     }
 }
 
@@ -68,10 +110,11 @@ console.log(list.size());
 // list.clear();
 // console.log(list.size())
 console.log(list.getFirst())
-console.log(list.removeFirst())
-console.log(list.removeFirst())
-console.log(list.removeFirst())
-console.log(list.removeFirst())
+// console.log(list.removeFirst())
+// console.log(list.removeFirst())
+// console.log(list.removeFirst())
+// console.log(list.removeFirst())
+console.log(list.removeLast())
 console.log(list.getLast())
 
 console.log(list)
