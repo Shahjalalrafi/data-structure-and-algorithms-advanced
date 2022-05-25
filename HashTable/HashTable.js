3106dfc23956f07404e6bc17795fd58a2695c41a
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-05-25 22:49:32 +0600

    find out keys from hash table data structure

--- HashTable/HashTable.js
+++ HashTable/HashTable.js
@@ -20,6 +20,7 @@ class HashTable {
             this.data[address] = [];
         }
 
+        console.log(address)
         this.data[address].push([key, value]);
         return this.data;
     }
@@ -31,15 +32,58 @@ class HashTable {
         if(currentBucket) {
             for(let i = 0; i< currentBucket.length; i++) {
                 if(currentBucket[i][0]) {
-                    console.log(currentBucket[i][1])
+                    return currentBucket[i][1]
                 }
             }
         }
     }
+
+
+    keys() {
+        if(!this.data.length) return undefined
+        
+        let allKeys = [];
+        for(let i = 0 ; i < this.data.length; i++) {
+            if(this.data[i]) {
+                allKeys.push(this.data[i][0][0]);
+            }
+        }
+
+        return allKeys
+    }
+
+
+    // // Another way to find keys
+    // keys() {
+    //     if(!this.data.length) return undefined
+        
+    //     let allKeys = [];
+    //     // loop through all the elements
+    //     for(let i = 0 ; i < this.data.length; i++) {
+    //          // if it's not an empty memory cell
+    //         if(this.data[i]) {
+    //             if(this.data[i].length > 1) {
+    //                 // but also loop through all the potential collisions
+    //                 for(let j = 0; j < this.data[i].length; j++) {
+    //                     allKeys.push(this.data[i][j][0]);
+    //                 }
+    //             }else {
+    //                 allKeys.push(this.data[i][0][0]);
+    //             }
+    //         }
+    //     }
+
+    //     return allKeys
+    // }
+
 }
 
 let myHashTable = new HashTable(10);
 // console.log(myHashTable._hash("grapes"));
 myHashTable.set("grapes", 1000);
 myHashTable.set("apple", 5000);
-myHashTable.get("apple")
+myHashTable.set("orange", 10);
+// myHashTable.set("banana", 100);
+myHashTable.get("apple");
+
+console.log(myHashTable.keys());
