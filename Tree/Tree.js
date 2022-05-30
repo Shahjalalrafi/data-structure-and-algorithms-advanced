fa9efd94166de50340154aacab9fa516763d5ecc
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-05-30 09:47:13 +0600

    tree Traverse depth  First

--- Tree/Tree.js
+++ Tree/Tree.js
@@ -12,6 +12,15 @@ class Tree {
             fn(node);
         }
     }
+
+    traverseDF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift();
+            arr.unshift(...node.children);
+            fn(node);
+        }
+    }
 }
 
 class Node {
