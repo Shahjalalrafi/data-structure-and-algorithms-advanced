8e3ca14ae4248b16858a9d9e83618f59e125dfc0
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-05-30 09:46:49 +0600

    tree Traverse Breadth First

--- Tree/Tree.js
+++ Tree/Tree.js
@@ -2,6 +2,16 @@ class Tree {
     constructor() {
         this.root = null;
     }
+
+    traverseBF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift();
+
+            arr.push(...node.children);
+            fn(node);
+        }
+    }
 }
 
 class Node {
