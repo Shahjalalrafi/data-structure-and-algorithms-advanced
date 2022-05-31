d4be3b3a0b4f938e320b4e1b9e61e7f70319cdfa
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-05-31 22:22:12 +0600

    another best implementation of chunkArray problem

--- ProblemSolving/ChunkArray.js
+++ ProblemSolving/ChunkArray.js
@@ -18,4 +18,17 @@ function chunkArray(arr, num) {
 }
 
 
+// Another best implementation 
+function chunkArray(array, size) {
+    const chunked = [];
+    let index = 0;
+
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+
+    return chunked;
+}
+
 console.log(chunkArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 19))
