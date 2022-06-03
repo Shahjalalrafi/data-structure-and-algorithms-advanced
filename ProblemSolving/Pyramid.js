49ad5bb37aec021e8cb5e8bf34c927a531a4b9ce
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-06-03 23:14:31 +0600

    pyramid problem solve in recursive way

--- ProblemSolving/Pyramid.js
+++ ProblemSolving/Pyramid.js
@@ -14,4 +14,29 @@ function pyramid(n) {
     }
 }
 
+
+// Recursive way Problem solve
+function pyramid(n, row = 0, level = "") {
+    if (row === n) {
+        return
+    }
+
+    if (level.length === 2 * n - 1) {
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
+
+    const midPoint = Math.floor((2 * n - 1) / 2);
+    let add
+
+    if (midPoint - row <= level.length && midPoint + row >= level.length) {
+        add = "#"
+    } else {
+        add = "-"
+    }
+
+    pyramid(n, row, level + add)
+}
+
+
 pyramid(5)
