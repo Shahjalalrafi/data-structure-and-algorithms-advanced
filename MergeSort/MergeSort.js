e403c82f703deb23c8b530efe21cac97c5ca1c3b
Shahjalalrafi <shahjalalrafi5@gmail.com>
2022-06-06 18:14:39 +0600

    Mergsort implemented in a another way

--- MergeSort/MergeSort.js
+++ MergeSort/MergeSort.js
@@ -1,40 +1,88 @@
 function merge(arr1, arr2) {
-    let result = []
-    let i = 0;
-    let j = 0;
+  let result = []
+  let i = 0;
+  let j = 0;
 
-    while(i < arr1.length && j < arr2.length) {
-        if(arr1[i] > arr2[j]) {
-            result.push(arr2[j])
-            j++
-        }else {
-            result.push(arr1[i])
-            i++
-        }
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] > arr2[j]) {
+      result.push(arr2[j])
+      j++
+    } else {
+      result.push(arr1[i])
+      i++
     }
+  }
 
-    while(i < arr1.length) {
-        result.push(arr1[i])
-        i++
-    }
+  while (i < arr1.length) {
+    result.push(arr1[i])
+    i++
+  }
 
-    while(j < arr2.length) {
-        result.push(arr2[j])
-        j++
-    }
+  while (j < arr2.length) {
+    result.push(arr2[j])
+    j++
+  }
 
-    return result
+  return result
 
 }
 
 function mergeSort(arr) {
-    if(arr.length <= 1) return arr
+  if (arr.length <= 1) return arr
 
-    let mid = Math.floor(arr.length/2)
-    let left  = mergeSort(arr.slice(0, mid))
-    let right = mergeSort(arr.slice(mid))
-    return merge(left, right)
+  let mid = Math.floor(arr.length / 2)
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+  return merge(left, right)
 }
 
 // console.log(merge([23, 44, 64], [7, 123, 444]))
 console.log(mergeSort([32, 66, 12, -44, 454]))
+
+
+// <====================================================================>
+// <================another way of implementation=======================>
+// <====================================================================>
+
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort(array) {
+  if (array.length === 1) {
+    return array
+  }
+
+  // Split Array in into right and left
+  const length = array.length;
+  const middle = Math.floor(length / 2)
+  const left = array.slice(0, middle)
+  const right = array.slice(middle)
+
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++
+    }
+
+  }
+  
+  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+
+const answer = mergeSort(numbers);
+console.log(answer);
