e374d66062b6d1669c23d3e05fe2fc94eb42739f
shahjalal rafi <shahjalalrafi5@gmail.com>
2022-06-28 00:01:18 +0600

    doubly linked list push implemented

--- Linkedlist/DoublyLinkedList.js
+++ Linkedlist/DoublyLinkedList.js
@@ -7,6 +7,30 @@ class Node {
 }
 
 
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(value) {
+        let newNode = new Node(value);
+
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+}
+
 
 let list = new DoublyLinkedList();
 
