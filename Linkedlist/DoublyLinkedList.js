d3d4c9509ee943648ea06d007d524d010d6a80bb
shahjalal rafi <shahjalalrafi5@gmail.com>
2022-06-28 21:44:51 +0600

    unShift implemented with Doubly Linked list

--- Linkedlist/DoublyLinkedList.js
+++ Linkedlist/DoublyLinkedList.js
@@ -17,10 +17,10 @@ class DoublyLinkedList {
     push(value) {
         let newNode = new Node(value);
 
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode;
             this.tail = newNode;
-        }else {
+        } else {
             this.tail.next = newNode;
             newNode.prev = this.tail;
             this.tail = newNode;
@@ -32,16 +32,16 @@ class DoublyLinkedList {
 
 
     pop() {
-        if(!this.head) {
+        if (!this.head) {
             return null;
         }
 
         let popedNode = this.tail;
 
-        if(this.length == 1) {
+        if (this.length == 1) {
             this.head = null;
             this.tail = null;
-        }else {
+        } else {
             this.tail = popedNode.prev;
             this.tail.next = null;
             popedNode.prev = null;
@@ -52,15 +52,15 @@ class DoublyLinkedList {
 
 
     shift() {
-        if(!this.head) {
+        if (!this.head) {
             return null;
         }
         let oldHead = this.head;
 
-        if(this.length === 1) {
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
-        }else {
+        } else {
             this.head = oldHead.next;
             oldHead.next = null;
             this.head.prev = null;
@@ -72,10 +72,10 @@ class DoublyLinkedList {
 
     unShift(value) {
         let newNode = new Node(value);
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode;
             this.tail = newNode;
-        }else {
+        } else {
             this.head.prev = newNode;
             newNode.next = this.head;
             this.head = newNode;
@@ -85,22 +85,57 @@ class DoublyLinkedList {
         return this;
     }
 
+
+    get(index) {
+        if (index < 0 || index >= this.length || !this.head) {
+            return null;
+        }
+
+        let current, count;
+        if (this.length / 2 >= index) {
+            console.log("start from beggining");
+            current = this.head;
+            count = 0;
+            
+            while (count !== index) {
+                current = current.next;
+                count++;
+            }
+        }else {
+            console.log("start from End");
+            current = this.tail;
+            count = this.length - 1;
+            
+            while(count !== index) {
+                current = current.prev;
+                count--;
+            }
+        }
+        return current;
+        
+    }
 }
 
 
 // 11 -><- 8 <--> 7 <--> 12 ->
 
 
+
 let list = new DoublyLinkedList();
 
-console.log(list.push(12));
-console.log(list.push(22));
-console.log(list.push(32));
+// console.log(list.push(12));
+// console.log(list.push(22));
+// console.log(list.push(32));
 // console.log(list.pop());
 // console.log(list.pop());
 // console.log(list.pop());
-console.log(list.shift());
-console.log(list.shift());
-console.log(list.shift());
-console.log(list.unShift(12));
-console.log(list.unShift(22));
+// console.log(list.shift());
+// console.log(list.shift());
+// console.log(list.shift());
+list.unShift(12);
+list.unShift(22);
+list.unShift(32);
+list.unShift(42);
+list.unShift(52);
+console.log(list.unShift(62));
+console.log(list.get(5))
